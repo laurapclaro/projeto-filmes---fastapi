babdd5965e79a1cdc3973627e02a3abb057c35d4
Laura <laurapapa693@gmail.com>
2025-10-21 08:47:16 -0300

    Minha mensagem de commit

--- back end/conexao.py
+++ back end/conexao.py
@@ -8,7 +8,7 @@ params = {
     "dbname": os.getenv("DB_NAME"),
     "user": os.getenv("DB_USER"),
     "password": os.getenv("DB_PASSWORD"),
-    "host": os.getenv("DB_HIST"),
+    "host": os.getenv("DB_HOST"),
     "port": os.getenv("DB_PORT")
 }
 
